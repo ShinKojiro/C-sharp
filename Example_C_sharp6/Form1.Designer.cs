3a932eb8bfa3162643eb519fe9bde09450f4212e
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-11 12:36:21 -0500

    Example 6

---
+++ Example_C_sharp6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Example_C_sharp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(362, 130);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblOutput.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(133, 130);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Text = "Type Here";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(365, 202);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(133, 201);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 3;
+            this.btnSwitch.Text = "&Switch";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnSwitch;
+    }
+}
+
---
+++ Example_C_sharp6/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_C_sharp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtInput.Text = "Type Here";
+
+            //focus ==> Select() or Focus()
+            txtInput.Select();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //formatting: f (float), n (number), p (percentages), c (currency)
+            double input = Convert.ToDouble(txtInput.Text);
+            lblOutput.Text = input.ToString("c");
+
+            MessageBox.Show("User Input: " + Environment.NewLine + input.ToString(), "Input", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+
+            txtInput.Text = "Type Here";
+            txtInput.Focus();
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            char userInput = Convert.ToChar(txtInput.Text);
+            
+
+            switch (userInput)
+            {
+                case 'A':
+                case 'a':
+                    lblOutput.Text = "Perfect Score!";
+                    break;
+                case 'f':
+                    lblOutput.Text = "Too Bad!";
+                    break;
+                default:
+                    lblOutput.Text = "Not expected";
+                    break;
+            }
+        }
+    }
+}
